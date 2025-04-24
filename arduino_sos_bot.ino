18f9bdf3f96e7ae7e2ed4856682e6c60bdad5994
Anara854 <kapanayeva_a@kt.nis.edu.kz>
2025-04-24 18:15:19 +0500

    Add files via upload

--- arduino_sos_bot.ino
+++ arduino_sos_bot.ino
@@ -1,24 +1,17 @@
-const int ledPin = 9;
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
-  Serial.println("Arduino готово");
-}
-
 void loop() {
   if (Serial.available()) {
+    Serial.println("👂 Данные пришли!");
     String input = Serial.readStringUntil('\n');
     input.trim();
 
-    Serial.print("Получено: ");
+    Serial.print("📥 Получено: ");
     Serial.println(input);
 
     if (input == "1") {
       digitalWrite(ledPin, HIGH);
       delay(1000);
       digitalWrite(ledPin, LOW);
-      Serial.println("OK");
+      Serial.println("💡 Лампа включена");
     }
   }
 }
